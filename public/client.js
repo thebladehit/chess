9b97272909bdbab01d4aa7952e5c08bd30042523
TheBladeHit <boqdanyarmolka@gmail.com>
2023-09-15 00:11:53 +0300

    Draw board when two player connected.

--- public/client.js
+++ public/client.js
@@ -1,3 +1,9 @@
+import Board from '/getFile?=game/board/board.js';
+import View from "/getFile?=game/view.js";
+import Game from "/getFile?=game/game.js";
+import { colors } from "/getFile?=game/resources/colors.js";
+import { defaultChessPosition } from "/getFile?=game/resources/position.js";
+
 const token = localStorage.getItem('CHESS_TOKEN');
 let game = null;
 
@@ -26,8 +32,61 @@ function redirect(url) {
   location.href = url;
 }
 
+function createGame(socket) {
+  socket.send(JSON.stringify({ event: 'gameCreate' }));
+}
+
+function waitingForPlayer() {
+  const userZone = document.querySelector('.table');
+  userZone.innerHTML = `<div class="alert">Waiting for player...</div>`;
+}
+
+function startChess(socket) { // here must draw chess field
+  document.querySelector('h1').remove();
+  document.querySelector('.table').remove();
+  const board = new Board(defaultChessPosition.cellNumberHorizontal, defaultChessPosition.cellNumberVertical);
+  board.addFigure(defaultChessPosition, colors.WHITE, colors.BLACK);
+  
+  const boardHtml = document.querySelector('.userZone');
+  const game = new Game(board);
+  const view = new View(boardHtml, game);
+  view.drawBoard();
+}
+
+const messageEvents = {
+  gamesList: (message, socket) => {
+    if (game) return;
+    createTrForTable(message.data, socket);
+  },
+  gameJoined: (message, socket) => {
+    game = message.data;
+    startChess(socket);
+  },
+  gameCreated: (message, socket) => {
+    game = message.data;
+    console.log(message.data);
+    waitingForPlayer();
+  },
+  gameCurrent: (message, socket) => {
+    game = message.data;
+    console.log(message.data);
+    waitingForPlayer();
+  }
+};
+
+function messageListener(message, socket) {
+  message = JSON.parse(message.data);
+  console.log(message);
+
+  for (const [eventName, event] of Object.entries(messageEvents)) {
+    if (eventName === message.event) {
+      event(message, socket);
+    }
+  }
+}
+
 function createTrForTable(games, socket) {
-  const userZone = document.querySelector('.userZone');
+  const userZone = document.querySelector('.table');
   userZone.innerHTML = `
     <div class="alert">
         Please, join to room or create new
@@ -63,57 +122,5 @@ function createTrForTable(games, socket) {
     })
     tr.lastChild.appendChild(btn);
     table.append(tr);
-  }
-}
-
-function createGame(socket) {
-  socket.send(JSON.stringify({ event: 'gameCreate' }));
-}
-
-function waitingForPlayer() {
-  const userZone = document.querySelector('.userZone');
-  userZone.innerHTML = `<div class="alert">Waiting for player...</div>`;
-}
-
-function startChess(socket) { // here must draw chess field
-  const userZone = document.querySelector('.userZone');
-  userZone.innerHTML = 'Game here';
-  const btn = document.createElement('button');
-  btn.textContent = 'Click';
-  btn.addEventListener('click', () => {
-    socket.send(JSON.stringify({ event: 'sendMove' }));
-  })
-  userZone.append(btn);
-}
-
-const messageEvents = {
-  gamesList: (message, socket) => {
-    if (game) return;
-    createTrForTable(message.data, socket);
-  },
-  gameJoined: (message, socket) => {
-    game = message.data;
-    startChess(socket);
-  },
-  gameCreated: (message, socket) => {
-    game = message.data;
-    console.log(message.data);
-    waitingForPlayer();
-  },
-  gameCurrent: (message, socket) => {
-    game = message.data;
-    console.log(message.data);
-    waitingForPlayer();
-  }
-};
-
-function messageListener(message, socket) {
-  message = JSON.parse(message.data);
-  console.log(message);
-
-  for (const [eventName, event] of Object.entries(messageEvents)) {
-    if (eventName === message.event) {
-      event(message, socket);
-    }
   }
 }
